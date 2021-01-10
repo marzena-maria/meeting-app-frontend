b6db37bd52dcfa2f47a522876ce2680eb3487b50
Marzena Studniarz <marzena.kupiec@gmail.com>
2021-01-11 00:32:57 +0100

    [event form created]

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 //import './App.scss';
 import React from "react"
-import {BrowserRouter,Route,Switch} from "react-router-dom"
-import Register from "./Forms/Register";
-import Login from "./Forms/Login";
-
+import {BrowserRouter,Route,Switch,Link} from "react-router-dom"
+import Register from "./Components/Forms/Register";
+import Login from "./Components/Forms/Login";
+import EventForm from './Components/Forms/EventForm/EventForm';
 
 function App() {
   return (
     <BrowserRouter>
 
+    <ul>
+      {/* <li>
+        <Link to="/">Homepage</Link>
+      </li> */}
+      <li>
+        <Link to='/add-new-event'>Add New Event</Link>
+      </li>
+    </ul>
+
     <Switch>
 
       <Route path ="/register" component={Register} exact />
       <Route path ="/login" component={Login} exact />
+      <Route path="/add-new-event" component={EventForm}/>
+      {/* <Route path="/" component={Homepage}/> */}
 
     </Switch>
 
