9f18041e35f9514363f86189be0562247e69f457
Marzena Studniarz <marzena.kupiec@gmail.com>
2021-01-13 00:53:31 +0100

    [DisplayData basic setup]

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
+import SearchFromInput from './SearchFromInput/SearchFromInput';
 
 function Home() {
     return (
         <div>
             <h1> Home Page </h1>
+
+            <SearchFromInput />
+            
         </div>
     )
 }
 
-export default Home
+export default Home;
