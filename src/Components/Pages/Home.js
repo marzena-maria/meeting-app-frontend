60a08e2d5afb31f18fd99ad4da5b53cba9900b8f
Marzena Studniarz <marzena.kupiec@gmail.com>
2021-01-25 11:40:25 +0100

    [Home file updated]

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -2,13 +2,14 @@ import React from "react";
 import NavBar from "../shared/NavBar";
 import Slider from "../Slider/Slider"
 import SearchFromInput from "./SearchFromInput/SearchFromInput";
+import SearchFromNav from './SearchFromNav/SearchFromNav';
 
 function Home() {
   return (
     <div>
       <NavBar />
       <Slider />
-
+      <SearchFromNav />
       <SearchFromInput />
     </div>
   );
