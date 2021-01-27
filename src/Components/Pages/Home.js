f61d8fc1aafeb37febedc3b6116402fd3740a87d
Marzena Studniarz <marzena.kupiec@gmail.com>
2021-01-27 09:38:53 +0100

    [maps settings]

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,20 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import NavBar from "../shared/NavBar"
-
+import Slider from '../Slider/Slider';
 import SearchFromInput from './SearchFromInput/SearchFromInput';
 import SearchFromNav from './SearchFromNav/SearchFromNav';
 import DisplayResults from '../shared/DisplayResults/DisplayResults';
+import Maps from './Maps/Maps';
 
 function Home() {
     const [events, setEvents] = React.useState([]);
     return (
         <div>
             <NavBar />  
-
+            <Slider />
             <SearchFromNav setEvents={setEvents} />
             <SearchFromInput setEvents={setEvents} />
             <DisplayResults 
                 listOfResults={events}/>
+            <Link to='/event-form'>Create New Event</Link>
+            <Maps />
+            
         </div>
     )
 
